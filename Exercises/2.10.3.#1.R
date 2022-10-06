87eed90f95017305369d320dc5a142352dfeb62c
RabbitMin <115143560+RabbitMin@users.noreply.github.com>
2022-10-06 18:47:16 +0900

    Add files via upload
    Read CpG island (CpGi) data from the compGenomRData package CpGi.table.hg18.txt. This is a tab-separated file. Store it in a variable called cpgi. Use
    cpgFilePath=system.file("extdata",
                    "CpGi.table.hg18.txt",
                    package="compGenomRData")
    to get the file path within the installed compGenomRData package. [Difficulty: Beginner]

---
+++ Exercises/2.10.3.#1.R
@@ -0,0 +1,6 @@
+cpgFilePath=system.file("extdata",
+                        "CpGi.table.hg18.txt",
+                        package="compGenomRData")
+
+cpgi <- read.table(cpgFilePath, header = FALSE, sep = "\t")
+
