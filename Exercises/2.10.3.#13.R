22d52d9191f3400870c4cc30104113bf6f6b47a4
RabbitMin <115143560+RabbitMin@users.noreply.github.com>
2022-10-06 19:14:36 +0900

    Add files via upload
    Use head() to see what is inside the data frames above. [Difficulty: Beginner]

---
+++ Exercises/2.10.3.#13.R
@@ -0,0 +1,13 @@
+refseqFilePath=system.file("extdata",
+                           "rn4.refseq.bed",
+                           package="compGenomRData")
+
+refseq2FilePath = system.file("extdata",
+                              "rn4.refseq2name.txt",
+                              package = "compGenomRData")
+
+df1 <- read.table(refseqFilePath, header = FALSE, sep = "\t")
+df2 <- read.table(refseq2FilePath, header = FALSE, sep = "\t")
+
+head(df1)
+head(df2)
