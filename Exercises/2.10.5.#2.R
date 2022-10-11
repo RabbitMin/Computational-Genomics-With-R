08d735e30cb83025213763e94986fec8a503c7e8
RabbitMin <115143560+RabbitMin@users.noreply.github.com>
2022-10-11 19:16:40 +0900

    Add files via upload
    Make a boxplot for the ‘perGc’ column. [Difficulty: Beginner]

---
+++ Exercises/2.10.5.#2.R
@@ -0,0 +1,7 @@
+cpgtFilePath=system.file("extdata",
+                         "CpGi.table.hg18.txt",
+                         package="compGenomRData")
+cpgi=read.table(cpgtFilePath,header=TRUE,sep="\t")
+head(cpgi)
+
+boxplot(cpgi$perGc, main = "perGc", ylab = "perGc")
