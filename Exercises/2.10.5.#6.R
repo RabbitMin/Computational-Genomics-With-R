188d1a030f6fbbd74224bc6e6d02cfab104847b7
RabbitMin <115143560+RabbitMin@users.noreply.github.com>
2022-10-11 19:31:49 +0900

    Add files via upload
    Use lapply to get GC percentage classes for gcValues. [Difficulty: Intermediate/Advanced]

---
+++ Exercises/2.10.5.#6.R
@@ -0,0 +1,12 @@
+gcValues = c(10, 50, 70, 65, 90)
+gcClass=function(x){
+  if(x < 60){
+    result = "low"
+  } else if(x > 75){
+    result = "high"
+  } else {
+    result = "medium"
+  }
+  result
+} 
+lapply(gcValues,gcClass)
